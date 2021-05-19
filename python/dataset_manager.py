63df398a0f0c6feb30b40cce5b478c3ea4c03b76
Flavio Forenza <flavio.forenza@studenti.unimi.it>
2021-05-19 23:52:29 +0200

    plot

--- python/dataset_manager.py
+++ python/dataset_manager.py
@@ -1,3 +1,5 @@
+import statistics
+
 import spacy
 import pymongo
 import matplotlib.pyplot as plt
@@ -6,9 +8,7 @@ import numpy as np
 from bson.objectid import ObjectId
 from gensim.parsing.preprocessing import remove_stopwords
 from collections import defaultdict
-from collections import Counter
 from tqdm import tqdm
-from matplotlib.pyplot import hist
 import seaborn as sns
 
 db = pymongo.MongoClient()["MIT"]["Recipes1M+"]
@@ -48,15 +48,33 @@ def counter(column):
 
 def plot_statistic(column):
     data = pd.read_pickle("./recipes.pkl")
-    sns_dis = sns.displot(data = data[column], kde=True)
-    sns_dis.savefig('imgs/displot ' + column)
-    plt.close()
-    sns_boxplot = sns.boxplot(y=data[column], linewidth=1, orient='v')
-    plt.savefig('imgs/boxplot ' + column)
-    plt.close()
+    fix, ax = plt.subplots(2,3, figsize=(10,10))
+    pos = [(0,0), (0,1), (0,2), (1,0), (1,1), (1,2)]
+    count = 0
+    for mod in column:
+        sns.distplot(data[mod].values, axlabel= mod, kde= False, ax=ax[pos[count]])
+        count += 1
+        sns.distplot(data[mod].values, axlabel= mod, kde=True, ax=ax[pos[count]])
+        count += 1
+        bbx = sns.boxplot(y=data[mod].values, linewidth=1, ax=ax[pos[count]])
+        count += 1
+    plt.tight_layout()
+    plt.savefig('imgs/displot')
+    plt.show()
+
     #DEBUG
     #sns.boxplot(y=counter(column))
 
-plot_statistic('totIngredients')
+data = pd.read_pickle("./recipes.pkl")
+fr = data['totIngredients'].values
+print("Median:", statistics.median(fr))
+print("Max:", fr.max())
+print("Mean:", statistics.mean(fr))
+sc = data['totInstructions'].values
+print("Median:", statistics.median(sc))
+print("Max:", sc.max())
+print("Mean:", statistics.mean(sc))
+plot_statistic(['totIngredients', 'totInstructions'])
+#plot_statistic('totInstructions')
 
 
